55ae06dfefda5052ba4805eea7ebe18235ce5478
Nikol Yosef <nikoli1601@gmail.com>
2026-07-12 17:05:48 +0300

    Update local API address

--- constants/api.ts
+++ constants/api.ts
@@ -3,4 +3,4 @@ import { Platform } from 'react-native';
 export const API_URL =
   Platform.OS === 'web'
     ? 'http://localhost:6001'
-    : 'http://192.168.1.145:6001';
+    : 'http://192.168.68.115:6001';
